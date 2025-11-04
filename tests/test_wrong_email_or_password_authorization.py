b632bdc390c75d7fc6570391af1e056519059eba
n-foll <n@foleeva.ru>
2025-11-04 22:27:55 +0300

    Параметризация, задание 4

--- tests/test_wrong_email_or_password_authorization.py
+++ tests/test_wrong_email_or_password_authorization.py
@@ -3,22 +3,26 @@ from playwright.sync_api import sync_playwright, expect, Page
 
 @pytest.mark.regression  # Добавили маркировку regression
 @pytest.mark.authorization  # Добавили маркировку authorization
-# Использование фикстуры 'chromium_page', которая автоматически предоставляет готовую страницу
-def test_wrong_email_or_password_authorization(chromium_page: Page):  # Создаем тестовую функцию
+@pytest.mark.parametrize("email, password", [
+    ("user.name@gmail.com", "password"),
+    ("user.name@gmail.com", "  "),
+    ("  ", "password")
+])
+def test_wrong_email_or_password_authorization(chromium_page: Page,  email: str, password: str):  # Создаем тестовую функцию
 
         chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
         email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-        email_input.fill("user.name@gmail.com")
+        email_input.fill(email)
 
         password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-        password_input.fill("password")
+        password_input.fill(password)
 
         login_button = chromium_page.get_by_test_id('login-page-login-button')
         login_button.click()
 
+
         wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
         expect(wrong_email_or_password_alert).to_be_visible()
         expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
-
 

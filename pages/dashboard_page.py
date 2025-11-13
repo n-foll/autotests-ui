a35d98c7a23f015a0c0f748a92f795e3177318c8
n-foll <n@foleeva.ru>
2025-11-13 22:26:18 +0300

    POM 3 задание

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,11 +1,17 @@
 from playwright.sync_api import Page, expect
 
 from pages.base_page import BasePage
+from components.navigation.navbar_component import NavbarComponent
+from components.navigation.sidebar_component import SidebarComponent
 
 
 class DashboardPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        self.navbar = NavbarComponent(page)
+        self.sidebar = SidebarComponent(page)
+
         self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
 
         self.students_title = page.get_by_test_id('students-widget-title-text')
@@ -24,20 +30,21 @@ class DashboardPage(BasePage):
         expect(self.dashboard_title).to_be_visible()
         expect(self.dashboard_title).to_have_text('Dashboard')
 
-    def check_visible_students_title(self):
+    def check_visible_students_chart(self):
         expect(self.students_title).to_be_visible()
         expect(self.students_title).to_have_text('Students')
         expect(self.students_chart).to_be_visible()
 
-    def check_visible_courses_title(self):
+    def check_visible_courses_chart(self):
         expect(self.courses_title).to_be_visible()
         expect(self.courses_title).to_have_text('Courses')
         expect(self.courses_chart).to_be_visible()
 
-    def check_visible_activities_title(self):
+    def check_visible_activities_chart(self):
         expect(self.activities_title).to_be_visible()
         expect(self.activities_title).to_have_text('Activities')
         expect(self.activities_chart).to_be_visible()
+
     def check_visible_scores_chart(self):
         expect(self.scores_title).to_be_visible()
         expect(self.scores_title).to_have_text('Scores')
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,4 +1,6 @@
 import pytest
+from playwright.sync_api import Page, expect
+
 from pages.create_course_page import CreateCoursePage
 from pages.courses_list_page import CoursesListPage
 
@@ -39,3 +41,15 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
         max_score="100",
         min_score="10"
     )
+
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()

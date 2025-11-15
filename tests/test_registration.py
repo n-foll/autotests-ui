8b3de0de70ef29132059a8b3a29bc10663220c1e
n-foll <n@foleeva.ru>
2025-11-15 22:44:54 +0300

    PageComponent 2 задание

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -9,4 +9,4 @@ def test_successful_registration(registration_page: RegistrationPage, dashboard_
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
     registration_page.registration_form.fill_registration()
     registration_page.click_registration_button()
-    dashboard_page.check_visible_dashboard_title()
+    dashboard_page.toolbar.check_visible()

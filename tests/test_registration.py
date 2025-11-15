f1a91d0787e971400424e6a3992bd7a0af2316af
n-foll <n@foleeva.ru>
2025-11-15 17:16:10 +0300

    PageComponent 2 задание

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -7,6 +7,6 @@ from pages.dashboard_page import DashboardPage
 @pytest.mark.registration  # Добавили маркировку registration
 def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.fill_registration_form()
+    registration_page.registration_form.fill_registration()
     registration_page.click_registration_button()
     dashboard_page.check_visible_dashboard_title()

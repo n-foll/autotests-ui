6d2fe627d5e306242792d15963d79c5da3e790f0
n-foll <n@foleeva.ru>
2025-11-15 15:51:04 +0300

    PageComponent

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -3,10 +3,9 @@ import pytest
 from pages.dashboard_page import DashboardPage
 
 
-
 @pytest.mark.dashboard
 @pytest.mark.regression
-def test_dashboard_displaying( dashboard_page_with_state: DashboardPage):
+def test_dashboard_displaying (dashboard_page_with_state: DashboardPage):
     dashboard_page_with_state.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
     dashboard_page_with_state.sidebar.check_visible()
     dashboard_page_with_state.navbar.check_visible("username")

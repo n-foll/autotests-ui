f99e644f3eb04beee09086c4cb434c64860fe5b7
n-foll <n@foleeva.ru>
2025-12-20 23:40:18 +0300

    Allure аннотации

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import Page,Locator, expect
-
+import allure
 
 class BaseElement:
     def __init__(self, page: Page, locator: str, name: str):
@@ -7,25 +7,33 @@ class BaseElement:
         self.name = name
         self.locator = locator
 
+    @property
+    def type_of(self) -> str:  # Добавили свойство type_of
+        return "base element"
+
         # Метод принимает кейворд аргументы (kwargs)
     def get_locator(self,nth:int = 0, **kwargs) -> Locator:  # объект Locator для взаимодействия с элементом
             # Инициализирует объект локатора, подставляя динамические значения в локатор.
             locator = self.locator.format(**kwargs)
-            # Возвращаем объект локатора
-            return self.page.get_by_test_id(locator).nth(nth)
+            with allure.step(f'Getting locator with "data-testid={locator}" at index "{nth}"'):
+                # Возвращаем объект локатора
+                return self.page.get_by_test_id(locator).nth(nth)
 
     def click(self,nth:int = 0, **kwargs):
-        # "Лениво" инициализируем локатор
-        locator = self.get_locator(nth, **kwargs)
-        # Выполняем нажатие на элемент
-        locator.click()
+        with allure.step(f'Clicking {self.type_of} "{self.name}"'):
+            # "Лениво" инициализируем локатор
+            locator = self.get_locator(nth, **kwargs)
+            # Выполняем нажатие на элемент
+            locator.click()
 
     def check_visible(self,nth:int = 0, **kwargs):
-        # Инициализируем локатор "лениво"
-        locator = self.get_locator(nth,**kwargs)
-        # Проверяем, что элемент виден на странице
-        expect(locator).to_be_visible()
+        with allure.step(f'Checking that {self.type_of} "{self.name}" is visible'):
+            # Инициализируем локатор "лениво"
+            locator = self.get_locator(nth,**kwargs)
+            # Проверяем, что элемент виден на странице
+            expect(locator).to_be_visible()
 
     def check_have_text(self, text: str,nth:int = 0, **kwargs):
-        locator = self.get_locator(nth,**kwargs)
-        expect(locator).to_have_text(text)
+        with allure.step(f'Checking that {self.type_of} "{self.name}" has text "{text}"'):
+            locator = self.get_locator(nth,**kwargs)
+            expect(locator).to_have_text(text)
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 from typing import Pattern
+import allure
 
 from playwright.sync_api import Page, expect  # Импортируем класс Page
 
@@ -8,11 +9,14 @@ class BasePage:
         self.page = page  # Присваиваем объект page атрибуту класса
 
     def visit(self, url: str):  # Метод для открытия ссылок
-        self.page.goto(url, wait_until='networkidle') #wait_until='networkidle' дожидаемся завершения загрузки всех сетевых запросов перед выполнением последующих шагов.
+        with allure.step(f'Opening the url "{url}"'):
+            self.page.goto(url, wait_until='networkidle') #wait_until='networkidle' дожидаемся завершения загрузки всех сетевых запросов перед выполнением последующих шагов.
 
     def reload(self):  # Метод для перезагрузки страницы
-        self.page.reload(wait_until='domcontentloaded') #wait_until='domcontentloaded' дожидаемся полного обновления страницы
+        with allure.step(f'Reloading page with url "{self.page.url}"'):
+         self.page.reload(wait_until='domcontentloaded') #wait_until='domcontentloaded' дожидаемся полного обновления страницы
 
     # Метод для проверки текущего URL
     def check_current_url(self, expected_url: Pattern[str]):
+        with allure.step(f'Checking that current url matches pattern "{expected_url.pattern}"'):
             expect(self.page).to_have_url(expected_url)

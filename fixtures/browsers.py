3a7ad84a661a500b4609791ff361825c8389d490
n-foll <n@foleeva.ru>
2026-01-22 23:05:26 +0300

    Практическое задание: запуск автотестов на нескольких браузерах

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -7,9 +7,13 @@ from tools.playwright.pages import initialize_playwright_page
 from config import settings
 
 
-@pytest.fixture
+@pytest.fixture (params=settings.browsers)
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        browser_type=request.param  # Передаем браузер как параметр
+    )
 
 
 @pytest.fixture(scope="session")
@@ -31,10 +35,11 @@ def initialize_browser_state(playwright: Playwright):
     browser.close()
 
 
-@pytest.fixture
+@pytest.fixture (params=settings.browsers)
 def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state=settings.browser_state_file  # Используем settings.browser_state_file
+        storage_state=settings.browser_state_file,  # Используем settings.browser_state_file
+        browser_type = request.param,  # Передаем браузер как параметр
     )

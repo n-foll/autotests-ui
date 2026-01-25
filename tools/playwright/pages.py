7b95cd9cb53d18bbd56601e2c0c0dc26546ff090
n-foll <n@foleeva.ru>
2026-01-25 22:19:46 +0300

    Fix gitignore and remove artifacts

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -18,7 +18,7 @@ def initialize_playwright_page(
         storage_state=storage_state,
         record_video_dir=settings.videos_dir
     )
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    context.tracing.start(screenshots=True, snapshots=False, sources=True)
     page = context.new_page()
     mock_static_resources(page)
 
